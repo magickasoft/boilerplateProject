7ba1a5ce9da31b9630454c85668878eb1a032e7b
Evgeny Shmakov <sjs-master@yandex.ru>
2019-09-13 20:20:56 +0700

    up config

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {RNHelp} from './screens';
+
+function wrapper() {
+  return <RNHelp />;
+}
+
+export default wrapper;
--- src/screens/RNHelp/index.js
+++ src/screens/RNHelp/index.js
@@ -1,30 +1,14 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Fragment} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
+import {SafeAreaView, ScrollView, View, Text, StatusBar} from 'react-native';
 import {
   Header,
   LearnMoreLinks,
-  Colors,
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import styles from './styles';
 
-const App = () => {
+const RNHelp = () => {
   return (
     <Fragment>
       <StatusBar barStyle="dark-content" />
@@ -72,43 +56,4 @@ const App = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+export default RNHelp;
