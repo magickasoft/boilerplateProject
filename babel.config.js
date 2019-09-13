be1de19533cbdb8bd045736ba19f228341dba201
Evgeny Shmakov <sjs-master@yandex.ru>
2019-09-13 21:02:09 +0700

    babel

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,28 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-transform-modules-commonjs',
+    [
+      'module-resolver',
+      {
+        extensions: ['.js', '.ios.js', '.android.js'],
+        root: ['./'],
+        alias: {
+          api: './src/api',
+          assets: './src/assets',
+          components: './src/components',
+          config: './src/config',
+          containers: './src/containers',
+          locales: './src/locales',
+          modules: './src/modules',
+          navigators: './src/navigators',
+          screens: './src/screens',
+          store: './src/store',
+          utils: './src/utils',
+        },
+      },
+    ],
+  ],
+  retainLines: true,
 };
